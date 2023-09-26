c93e5c9daebcda2ca849315cd02280231bfa21dc
Nirdhum <66917558+nirdhum@users.noreply.github.com>
2023-09-27 02:07:32 +0530

    implemented mobile nav

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
+
+const mobile_nav = document.querySelector('.mobile-navbar-btn')
+
+const nav_header = document.querySelector('header')
+
+const nav_opt = document.querySelector('.nav-opt')
+
+const toggleNav = () => {
+    nav_header.classList.toggle('active')
+}
+
+mobile_nav.addEventListener('click', () => toggleNav())
+
+nav_opt.addEventListener('click', () => toggleNav())
