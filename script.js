29c2692a6238ab02ee3060c1d8b1f32027354096
Nirdhum <66917558+nirdhum@users.noreply.github.com>
2023-10-08 22:08:19 +0530

    Added dark/light mode functionality

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ const toggleNav = () => {
 mobile_nav.addEventListener('click', () => toggleNav())
 
 nav_opt.addEventListener('click', () => toggleNav())
+
+const toogle_btn = document.getElementById('checkbox')
+const root = document.querySelector(':root')
+
+toogle_btn.addEventListener('change', () => {
+    if (toogle_btn.checked) {
+        root.style.setProperty('--primary', '#cbcff6')
+        root.style.setProperty('--white', '#1B2430')
+        root.style.setProperty('--black', '#FFF')
+        root.style.setProperty('--dark', '#FFF0F5')
+        root.style.setProperty('--light', '#16213E')
+    }
+    else {
+        root.style.setProperty('--primary', '#525FE1')
+        root.style.setProperty('--white', '#FFF')
+        root.style.setProperty('--black', '#1B2430')
+        root.style.setProperty('--dark', '#16213E')
+        root.style.setProperty('--light', '#FFF0F5')
+    }
+})
